7dedc7e4cdee753a78a41bde492629ebaea78199
ajborbon <ajborbon@unal.edu.co>
2025-02-27 09:40:18 -0500

    fix[Notion]: Entregable ya copia el ID de publicacion en Notion de la nota de Proyecto Campaña si se define que es la padre.

--- src/modules/noteLifecycleManager/fieldHandlers/FH Subsistemas/EntregableFieldHandler.ts
+++ src/modules/noteLifecycleManager/fieldHandlers/FH Subsistemas/EntregableFieldHandler.ts
@@ -16,6 +16,227 @@ export class EntregableFieldHandler extends NoteFieldHandler implements Entregab
         super(tp, infoSubsistema, plugin);
     }
     
+      // Sobrescribimos el método getAsunto para manejar NotionID
+      async getAsunto(): Promise<{ siAsunto: boolean, nombre: string[] }> { 
+        let padres = [];
+        let activo = app.workspace.getActiveFile();
+        let siAsunto = false;
+        let nombre = "";
+        let nota;
+        
+        if (activo != null) {
+            nombre = activo.basename;
+            nota = app.metadataCache.getFileCache(activo);
+            // Asegurar que nota.frontmatter existe y asignar un objeto vacío a file
+            nota.frontmatter = nota.frontmatter || {};
+            nota.frontmatter.file = {};
+            Object.assign(nota.frontmatter.file, activo);
+            
+            padres.unshift(nombre); // Añade el nombre al inicio del arreglo, desplazando los demás elementos
+            siAsunto = await this.suggester(["Si", "No"], [true, false], true, nombre + " es origen de " + this.nota.titulo + "?");
+            
+            if (siAsunto) {
+                this.nota.asuntoDefinido = true; // Para que no ejecute la busqueda de Area Vida, Area de Interés, proyecto Q o GTD
+                
+                // Verificamos si existe un campo que comience con "NotionID-" en el frontmatter
+                let notionID = null;
+                
+                for (const key in nota.frontmatter) {
+                    if (key.startsWith("NotionID-")) {
+                        notionID = nota.frontmatter[key];
+                        console.log(`Encontrado NotionID: ${notionID} con clave: ${key}`);
+                        break;
+                    }
+                }
+                
+                // Si encontramos un NotionID, lo guardamos en el campo proyecto
+                if (notionID) {
+                    this.nota.proyecto = [notionID];
+                    console.log(`Asignado NotionID al proyecto: ${notionID}`);
+                }
+                
+                let nivel;
+                switch (nota?.frontmatter?.type) { // La nota activa es una:
+                    default:
+                    case "PGTD":
+                    case "PQ":
+                        // VERIFICACION DE PROYECTOS DE Q Y PROYECTO GTD
+                        if (nota.frontmatter?.type === "PQ") {
+                            // CUANDO LA NOTA ACTIVA ES UN PQ.
+                            this.nota.proyectoQ = nombre;
+                            // VERIFICACION DE PROYECTOSGTD
+                            // Inicializamos this.nota.proyectoGTD con un valor predeterminado de cadena vacía
+                            this.nota.proyectoGTD = "";
+                            // Verificamos si nota.proyectoGTD existe y es un arreglo
+                            if (Array.isArray(nota.frontmatter.proyectoGTD)) {
+                                // Si es un arreglo, iteramos sobre cada elemento
+                                this.nota.proyectoGTD = nota.frontmatter.proyectoGTD.map(elemento =>
+                                    elemento.replace(/\[\[\s*|\s*\]\]/g, ''));
+                            } else if (nota.frontmatter.proyectoGTD) {
+                                // Si existe pero no es un arreglo, aplicamos el regex directamente
+                                this.nota.proyectoGTD = nota.frontmatter.proyectoGTD.replace(/\[\[\s*|\s*\]\]/g, '');
+                            }
+                            // Si nota.proyectoGTD no existe, this.nota.proyectoGTD ya está establecido en "" por defecto
+
+
+                            // Obtener ProyectoQ y Proyecto GTD cuando la nota es ProyectoGTD.
+                        } else if (nota.frontmatter?.type === "PGTD") {
+
+                            // CUANDO LA NOTA ACTIVA ES UN GTD.
+                            // VERIFICACION DE PROYECTOSGTD
+                            this.nota.proyectoGTD = [nombre];
+
+                            if (Array.isArray(nota.frontmatter.proyectoGTD)) {
+                                // Si es un arreglo, utilizamos concat para añadir los elementos ya procesados con el regex al arreglo this.nota.proyectoGTD
+                                this.nota.proyectoGTD = this.nota.proyectoGTD.concat(nota.frontmatter.proyectoGTD.map(elemento =>
+                                    elemento.replace(/\[\[\s*|\s*\]\]/g, '')));
+                            } else if (nota.frontmatter.proyectoGTD) {
+                                // Si existe pero no es un arreglo, aplicamos el regex directamente y usamos push para agregarlo a this.nota.proyectoGTD
+                                this.nota.proyectoGTD.push(nota.frontmatter.proyectoGTD.replace(/\[\[\s*|\s*\]\]/g, ''));
+                            }
+
+                            // Si nota.proyectoGTD no existe, this.nota.proyectoGTD ya está establecido en "" por defecto
+                            this.nota.proyectoQ = "";
+                            if (Array.isArray(nota.frontmatter.proyectoQ)) {
+                                // Si es un arreglo, iteramos sobre cada elemento
+                                this.nota.proyectoQ = nota.frontmatter.proyectoQ.map(elemento =>
+                                    elemento.replace(/\[\[\s*|\s*\]\]/g, ''));
+                            } else if (nota.frontmatter.proyectoQ) {
+                                // Si existe pero no es un arreglo, aplicamos el regex directamente
+                                this.nota.proyectoQ = nota.frontmatter.proyectoQ.replace(/\[\[\s*|\s*\]\]/g, '');
+                            }
+
+
+                        }
+                        // Obtener ProyectoQ y Proyecto GTD cuando la nota es otra cosa que no es proyecto
+                        else {
+
+                            this.nota.proyectoQ = "";
+                            if (Array.isArray(nota?.frontmatter?.proyectoQ)) {
+                                // Si es un arreglo, iteramos sobre cada elemento
+                                this.nota.proyectoQ = nota.frontmatter.proyectoQ.map(elemento =>
+                                    elemento.replace(/\[\[\s*|\s*\]\]/g, ''));
+                            } else if (nota?.frontmatter?.proyectoQ) {
+                                // Si existe pero no es un arreglo, aplicamos el regex directamente
+                                this.nota.proyectoQ = nota.frontmatter.proyectoQ.replace(/\[\[\s*|\s*\]\]/g, '');
+                            }
+
+                            this.nota.proyectoGTD = "";
+                            // Verificamos si nota.proyectoGTD existe y es un arreglo
+                            if (Array.isArray(nota?.frontmatter?.proyectoGTD)) {
+                                // Si es un arreglo, iteramos sobre cada elemento
+                                this.nota.proyectoGTD = nota.frontmatter.proyectoGTD.map(elemento =>
+                                    elemento.replace(/\[\[\s*|\s*\]\]/g, ''));
+                            } else if (nota?.frontmatter?.proyectoGTD) {
+                                // Si existe pero no es un arreglo, aplicamos el regex directamente
+                                this.nota.proyectoGTD = nota.frontmatter.proyectoGTD.replace(/\[\[\s*|\s*\]\]/g, '');
+                            }
+
+                        }
+                        // Verificamos areaInteres 
+                        this.nota.areaInteres = [];
+                        if (Array.isArray(nota?.frontmatter?.areaInteres)) {
+                            // Si es un arreglo, iteramos sobre cada elemento (excluyendo el primer elemento ya agregado que es nota.titulo)
+                            // y aplicamos el regex a cada elemento. Luego concatenamos con el array existente.
+                            this.nota.areaInteres = this.nota.areaInteres.concat(nota.frontmatter.areaInteres.map(elemento =>
+                                elemento.replace(/\[\[\s*|\s*\]\]/g, '')));
+                        } else {
+                            // Si no es un arreglo, revisamos si nota.frontmatter.areaInteres existe
+                            if (nota?.frontmatter?.areaInteres) {
+                                // Si existe, aplicamos el regex y lo añadimos como segundo elemento
+                                this.nota.areaInteres.push(nota.frontmatter.areaInteres.replace(/\[\[\s*|\s*\]\]/g, ''));
+                            }
+                        }
+                        // Verificamos AreaVida
+                        this.nota.areaVida = "";
+                        if (nota?.frontmatter?.areaVida) {
+                            if (Array.isArray(nota.frontmatter.areaVida)) {
+                                // Es un arreglo, usa el primer elemento
+                                this.nota.areaVida = nota.frontmatter.areaVida[0].replace(/\[\[\s*|\s*\]\]/g, '');
+                            } else if (typeof nota.frontmatter.areaVida === 'string') {
+                                // Es un string
+                                this.nota.areaVida = nota.frontmatter.areaVida.replace(/\[\[\s*|\s*\]\]/g, '');
+                            }
+                        } else {
+                            // No está definido o está vacío
+                            this.nota.areaVida = "No es de ningún Area de Vida";
+                        }
+
+                        // DEFINIR NIVELP
+                        // Comprueba si nivelAI existe y no es NaN después de intentar convertirlo a entero
+
+                        if (!isNaN(parseInt(nota?.frontmatter?.nivelP))) {
+                            nivel = parseInt(nota.frontmatter.nivelP) + 1;
+                        } else {
+                            // Si nivelAI no existe o su conversión a entero resulta en NaN, establece nivel a 0
+                            nivel = 0;
+                        }
+                        this.nota.nivelP = nivel;
+
+                        break; // PGTD y PQ
+                    case "AI":
+                        // VERIFICACION DE AREA DE INTERES
+                        if (nota?.frontmatter?.type === "AI") {
+                            this.nota.areaInteres = [nota.frontmatter.file.basename];
+                            // Inicializamos this.nota.areaInteres con nota.titulo como el primer elemento.
+                            // Este solo aplica para cuando estoy construyendo desde Area de Interes otra Area de Interes.
+                        }
+                        // Verificamos si nota.areaInteres es un arreglo
+                        if (Array.isArray(nota.frontmatter?.areaInteres)) {
+                            // Si es un arreglo, iteramos sobre cada elemento (excluyendo el primer elemento ya agregado que es nota.titulo)
+                            // y aplicamos el regex a cada elemento. Luego concatenamos con el array existente.
+                            this.nota.areaInteres = this.nota.areaInteres.concat(nota.frontmatter.areaInteres.map(elemento =>
+                                elemento.replace(/\[\[\s*|\s*\]\]/g, '')));
+                        } else {
+                            // Si no es un arreglo, revisamos si nota.frontmatter.areaInteres existe
+                            if (nota?.frontmatter.areaInteres) {
+                                // Si existe, aplicamos el regex y lo añadimos como segundo elemento
+                                this.nota.areaInteres.push(nota.frontmatter.areaInteres.replace(/\[\[\s*|\s*\]\]/g, ''));
+                            } else {
+                                // Si no es arreglo ni string, areaInteres es el area interes que está en titulo.   
+                            }
+                            // Si nota.frontmatter.areaInteres no existe, this.nota.areaInteres ya tendrá nota.titulo como su único elemento
+                        }
+                    case "AV":
+                        // VERIFICACION DE AREA DE VIDA
+                        if (nota?.frontmatter?.areaVida) {
+                            if (Array.isArray(nota.frontmatter.areaVida)) {
+                                // Es un arreglo, usa el primer elemento
+                                this.nota.areaVida = nota.frontmatter.areaVida[0].replace(/\[\[\s*|\s*\]\]/g, '');
+                            } else if (typeof nota.frontmatter.areaVida === 'string') {
+                                // Es un string
+                                this.nota.areaVida = nota.frontmatter.areaVida.replace(/\[\[\s*|\s*\]\]/g, '');
+                            }
+                        } else {
+                            // No está definido o está vacío
+                            this.nota.areaVida = "No es de ningún Area de Vida";
+                        }
+                        // poniendo si Asunto en false para las notas estructura AI y AV. 
+                        if (nota?.frontmatter?.type === "AI" || nota?.frontmatter?.type === "AV") {
+                            siAsunto = false;
+                        }
+
+                        // DEFINIR NIVELP
+                        // Comprueba si nivelAI existe y no es NaN después de intentar convertirlo a entero
+
+                        if (!isNaN(parseInt(nota.frontmatter?.nivelP))) {
+                            nivel = parseInt(nota.frontmatter.nivelP) + 1;
+                        } else {
+                            // Si nivelAI no existe o su conversión a entero resulta en NaN, establece nivel a 0
+                            nivel = 0;
+                        }
+                        this.nota.nivelP = nivel;
+                        break;
+                }
+            } else { // activa no es origen de Creando RR - PGTD - PQ  if(siAsunto)
+
+            }
+        } else { // activo == null
+            siAsunto = false;
+        }
+        return { siAsunto, nombre: padres }
+    }
+    
     async getTrimestre(): Promise<string> {
         // Verificar si es una continuación desde una Campaña
         const activo = app.workspace.getActiveFile();
